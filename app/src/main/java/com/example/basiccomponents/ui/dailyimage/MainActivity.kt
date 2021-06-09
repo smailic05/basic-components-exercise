c14255792175ab3eb94ae8785d51b014a500ebf0
smailic05 <murad0660@gmail.com>
2021-06-09 19:55:56 +0300

    feat: add ui

--- app/src/main/java/com/example/basiccomponents/ui/dailyimage/MainActivity.kt
+++ app/src/main/java/com/example/basiccomponents/ui/dailyimage/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.basiccomponents.ui.dailyimage
 
+import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import coil.load
+import com.example.basiccomponents.R
 import com.example.basiccomponents.network.repo.NasaRepository
 import com.example.basiccomponents.ui.models.NasaDailyImage
+import com.google.android.material.switchmaterial.SwitchMaterial
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 import java.util.*
@@ -18,13 +22,34 @@ class MainActivity : AppCompatActivity() {
      */
     private val nasaRepository by inject<NasaRepository>()
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
-    private val exceptionHandler = CoroutineExceptionHandler { _, throwable -> handleError(throwable) }
+    private val exceptionHandler =
+        CoroutineExceptionHandler { _, throwable -> handleError(throwable) }
 
     //TODO: initialize your views and other fields here
     private var nasaImage: ImageView? = null
+    private var dateView: TextView? = null
+    private var explainView: TextView? = null
+    private var switchMaterial: SwitchMaterial? = null
+    private var toolbar: androidx.appcompat.widget.Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        nasaImage = findViewById(R.id.image_view)
+        dateView = findViewById(R.id.date_view)
+        switchMaterial = findViewById(R.id.switchTheme)
+        explainView = findViewById(R.id.explain_view)
+        toolbar = findViewById(R.id.toolbar)
+        fetchDailyImage(Date())
+        switchMaterial?.setOnClickListener { changeTheme() }
+    }
+
 
     private fun initViews(data: NasaDailyImage) {
         nasaImage?.load(data.imageUrl)
+        dateView?.text = data.date
+        explainView?.text = data.explanation
+        toolbar?.title = data.title
         //TODO: apply data to your views
     }
 
@@ -32,12 +57,6 @@ class MainActivity : AppCompatActivity() {
         Log.e(MainActivity::class.java.simpleName, "exception!", t)
         //TODO: handle request's errors here
     }
-
-
-
-
-
-
 
     /**
      * Don't edit this function. Use only for fetching data.
